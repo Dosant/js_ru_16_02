22159a788da7568b4d4957d41d8a499e85e80cf7
AntonDosov <Dosantinc@gmail.com>
2016-03-14 22:37:45 +0300

    add react-selector, show article if selected

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,9 +3,13 @@ import { articlesStore, commentStore } from './../stores'
 import ArticleList from './ArticleList'
 import { loadAllArticles } from './../actions/articles'
 
+import Select from 'react-select';
+import selectStyles from 'react-select/dist/react-select.css'
+
 class Container extends Component {
     state = {
         articles: articlesStore.getOrLoadAll(),
+        selected: [],
         loading: true
     }
 
@@ -22,11 +26,47 @@ class Container extends Component {
     render() {
         const { articles, loading } = this.state
         if (loading) return <h3>Loading...</h3>
+
+        const selectedArticles = this.getSelectedArticles(articles);
         return (
             <div>
-                <ArticleList articles = {articles} />
+                {this.getSelector(articles)}
+                {selectedArticles.length === 0 ?
+                    <span>Select Articles</span> :
+                    <ArticleList articles = {selectedArticles} />}
             </div>
         )
+    }
+
+    getSelectedArticles (articles) {
+        return articles.filter((article) => {
+            return this.state.selected.find((selected) => {
+                return selected.value === article.id;
+            });
+        });
+    }
+
+    getSelector (articles) {
+        let options = articles.map((article) => {
+            return {
+                value: article.id,
+                label: article.title
+            }
+        });
+
+        function onSelection(val, selected) {
+            this.setState({
+                selected: selected
+            });
+        };
+
+        return <Select
+            name="form-field-name"
+            value={this.state.selected}
+            options={options}
+            multi={true}
+            onChange={onSelection.bind(this)}
+        />
     }
 
     change = () => {
